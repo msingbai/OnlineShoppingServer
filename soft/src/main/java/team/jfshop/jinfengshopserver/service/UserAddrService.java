1ff1dca3e9b3246e91f34514c43bd64e875c63a4
msingbai <895663730@qq.com>
2022-12-18 20:07:09 +0800

    引入配置Swagger依赖

---
+++ soft/src/main/java/team/jfshop/jinfengshopserver/service/UserAddrService.java
@@ -0,0 +1,8 @@
+package team.jfshop.jinfengshopserver.service;
+
+import team.jfshop.jinfengshopserver.vo.ResultVO;
+
+public interface UserAddrService {
+
+    public ResultVO listAddrsByUid(int userId);
+}
