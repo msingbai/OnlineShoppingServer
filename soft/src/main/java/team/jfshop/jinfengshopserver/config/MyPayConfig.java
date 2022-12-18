fb90592da18583cdf29e31df497628c26c2ae13d
msingbai <895663730@qq.com>
2022-12-18 20:54:14 +0800

    支付config

---
+++ soft/src/main/java/team/jfshop/jinfengshopserver/config/MyPayConfig.java
@@ -0,0 +1,39 @@
+package team.jfshop.jinfengshopserver.config;
+
+import team.jfshop.jinfengshopserver.wxpay.WXPayConfig;
+
+import java.io.InputStream;
+
+
+public class MyPayConfig implements WXPayConfig {
+    @Override
+    public String getAppID() {
+        return "wx632c8f211f8122c6";
+    }
+
+    @Override
+    public String getMchID() {
+        return "1497984412";
+    }
+
+    @Override
+    public String getKey() {
+        return "sbNCm1JnevqI36LrEaxFwcaT0hkGxFnC";
+    }
+
+    @Override
+    public InputStream getCertStream() {
+        return null;
+    }
+
+    @Override
+    public int getHttpConnectTimeoutMs() {
+        return 0;
+    }
+
+    @Override
+    public int getHttpReadTimeoutMs() {
+        return 0;
+    }
+}
+
